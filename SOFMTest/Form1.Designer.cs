bd99aeaddd393ba6846019e20dcff46ec3c9055d
Rey Joshua Macarat <reyjoshua.macarat@cit.edu>
2021-02-25 16:56:56 +0000

    Add project files.

---
+++ SOFMTest/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace SOFMTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.panelLegend = new System.Windows.Forms.Panel();
+            this.btnLoadSingle = new System.Windows.Forms.Button();
+            this.btnLoadData = new System.Windows.Forms.Button();
+            this.gbKohonen = new System.Windows.Forms.GroupBox();
+            this.panelStatus = new System.Windows.Forms.Panel();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.sofmVisualizer = new SOFMTest.SOFMVisualizer();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.gbKohonen.SuspendLayout();
+            this.panelStatus.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.panelLegend);
+            this.splitContainer1.Panel1.Controls.Add(this.lblResult);
+            this.splitContainer1.Panel1.Controls.Add(this.btnLoadSingle);
+            this.splitContainer1.Panel1.Controls.Add(this.btnLoadData);
+            this.splitContainer1.Panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.gbKohonen);
+            this.splitContainer1.Size = new System.Drawing.Size(1468, 1117);
+            this.splitContainer1.SplitterDistance = 437;
+            this.splitContainer1.SplitterWidth = 6;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // panelLegend
+            // 
+            this.panelLegend.AutoSize = true;
+            this.panelLegend.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panelLegend.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelLegend.Location = new System.Drawing.Point(32, 807);
+            this.panelLegend.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panelLegend.Name = "panelLegend";
+            this.panelLegend.Size = new System.Drawing.Size(2, 2);
+            this.panelLegend.TabIndex = 5;
+            this.panelLegend.Visible = false;
+            // 
+            // btnLoadSingle
+            // 
+            this.btnLoadSingle.BackColor = System.Drawing.SystemColors.Control;
+            this.btnLoadSingle.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnLoadSingle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLoadSingle.Location = new System.Drawing.Point(75, 189);
+            this.btnLoadSingle.Name = "btnLoadSingle";
+            this.btnLoadSingle.Size = new System.Drawing.Size(255, 56);
+            this.btnLoadSingle.TabIndex = 12;
+            this.btnLoadSingle.Text = "Load Single Data";
+            this.btnLoadSingle.UseVisualStyleBackColor = false;
+            this.btnLoadSingle.Click += new System.EventHandler(this.btnLoadWeight_Click);
+            // 
+            // btnLoadData
+            // 
+            this.btnLoadData.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLoadData.Location = new System.Drawing.Point(75, 96);
+            this.btnLoadData.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnLoadData.Name = "btnLoadData";
+            this.btnLoadData.Size = new System.Drawing.Size(255, 54);
+            this.btnLoadData.TabIndex = 11;
+            this.btnLoadData.Text = "Load new dataset";
+            this.btnLoadData.UseVisualStyleBackColor = true;
+            this.btnLoadData.Click += new System.EventHandler(this.btnLoadDataAndCreateNetwork_Click);
+            // 
+            // gbKohonen
+            // 
+            this.gbKohonen.AutoSize = true;
+            this.gbKohonen.Controls.Add(this.panelStatus);
+            this.gbKohonen.Controls.Add(this.sofmVisualizer);
+            this.gbKohonen.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gbKohonen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.gbKohonen.Location = new System.Drawing.Point(0, 0);
+            this.gbKohonen.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gbKohonen.Name = "gbKohonen";
+            this.gbKohonen.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gbKohonen.Size = new System.Drawing.Size(1025, 1117);
+            this.gbKohonen.TabIndex = 0;
+            this.gbKohonen.TabStop = false;
+            this.gbKohonen.Text = "Kohonen map";
+            // 
+            // panelStatus
+            // 
+            this.panelStatus.BackColor = System.Drawing.SystemColors.MenuText;
+            this.panelStatus.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelStatus.Controls.Add(this.lblTime);
+            this.panelStatus.Location = new System.Drawing.Point(-18, 1056);
+            this.panelStatus.Name = "panelStatus";
+            this.panelStatus.Size = new System.Drawing.Size(1043, 62);
+            this.panelStatus.TabIndex = 6;
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblTime.Location = new System.Drawing.Point(351, 23);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(186, 20);
+            this.lblTime.TabIndex = 6;
+            this.lblTime.Text = "Time Elapsed: 0 seconds";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblResult.Location = new System.Drawing.Point(71, 610);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(55, 20);
+            this.lblResult.TabIndex = 6;
+            this.lblResult.Text = "Result";
+            // 
+            // sofmVisualizer
+            // 
+            this.sofmVisualizer.Location = new System.Drawing.Point(4, 32);
+            this.sofmVisualizer.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.sofmVisualizer.Matrix = null;
+            this.sofmVisualizer.Name = "sofmVisualizer";
+            this.sofmVisualizer.Size = new System.Drawing.Size(1021, 1016);
+            this.sofmVisualizer.TabIndex = 3;
+            this.sofmVisualizer.ZoomFactor = 25;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1468, 1117);
+            this.Controls.Add(this.splitContainer1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "SOFM Demo Project";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.gbKohonen.ResumeLayout(false);
+            this.panelStatus.ResumeLayout(false);
+            this.panelStatus.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox gbKohonen;
+        private System.Windows.Forms.Button btnLoadData;
+        private System.Windows.Forms.ToolTip toolTip;
+        private SOFMVisualizer sofmVisualizer;
+        private System.Windows.Forms.Panel panelLegend;
+        private System.Windows.Forms.Button btnLoadSingle;
+        private System.Windows.Forms.Panel panelStatus;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblTime;
+    }
+}
+
---
+++ SOFMTest/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using SOFM;
+using System.Diagnostics;
+
+namespace SOFMTest
+{
+    public partial class Form1 : Form
+    {
+        private SOFM.NeuralNetwork nn;
+        private readonly Stopwatch sw;
+        private float[,] centroids;
+
+        public Form1()
+        {
+            InitializeComponent();
+            sw = new Stopwatch();
+            showLegend();
+        }
+
+        private void computeCentroids()
+        {
+            for (int x = 0; x < nn.UsedColors.Count; x++)
+            {
+                Color temp = nn.UsedColors[x];
+                float xValues = 0;
+                float yValues = 0;
+                int totalPoints = 0;
+                for (int i = 0; i < nn.OutputLayerDimension; i++)
+                {
+                    for(int j = 0; j < nn.OutputLayerDimension; j++)
+                    {
+                        if (sofmVisualizer.Matrix[i,j] == temp)
+                        {
+                            xValues += i;
+                            yValues += j;
+                            totalPoints++;
+                        }
+                    }
+                }
+                centroids[x, 0] = xValues / totalPoints;
+                centroids[x, 1] = yValues / totalPoints;
+
+            }
+        }
+
+        private string findNearestDistance(int x, int y)
+        {
+            double distance = 999999;
+            int centroid_index = 0;
+            for (int i = 0; i < nn.UsedColors.Count; i++)
+            {
+                double tempDistance = Math.Sqrt((double)(Math.Pow(centroids[i, 0] - x, 2) + Math.Pow(centroids[i, 1] - y, 2)));
+                if (tempDistance < distance)
+                {
+                    distance = tempDistance;
+                    centroid_index = i;
+                }
+            }
+
+            return nn.ExistentClasses.Keys[centroid_index];
+        }
+        private void nn_EndIterationEvent(object sender, EventArgs e)
+        {
+            lblTime.Text = "Time Elapsed: " + sw.Elapsed.Seconds.ToString() + " seconds";
+        }
+
+        private void btnLoadDataAndCreateNetwork_Click(object sender, EventArgs e)
+        {
+            nn = new NeuralNetwork(25, 500, 0.00000001, f: Functions.MexicanHat);
+            nn.EndEpochEvent += new EndEpochEventHandler(nn_EndEpochEvent);
+            nn.EndIterationEvent += new EndIterationEventHandler(nn_EndIterationEvent);
+            nn.Normalize = true;
+            
+            
+            OpenFileDialog ofd = new OpenFileDialog();
+            
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                nn.ReadDataFromFile(ofd.FileName);
+                sofmVisualizer.Matrix = null;
+                sw.Restart();
+                nn.StartLearning();
+                sofmVisualizer.Matrix = nn.ColorSOFM();
+                centroids = new float[nn.UsedColors.Count, 2];
+                computeCentroids();
+                sofmVisualizer.Invalidate();
+                sw.Stop();
+                AddLegend();
+            }
+        }
+
+        private void showLegend()
+        {
+            panelLegend.Controls.Clear();
+            Label label = new Label
+            {
+                Name = "lblLegend",
+                Top = 5,
+                Left = 5,
+                Text = "Groups",
+                AutoSize = true
+            };
+            this.panelLegend.Controls.Add(label);
+            panelLegend.Visible = true;
+        }
+
+        private void AddLegend()
+        {
+            this.panelLegend.Controls.Clear();
+            for (int i = 0; i < nn.ExistentClasses.Count; i++)
+            {
+                Label lbl = new Label
+                {
+                    Name = "lbl" + nn.ExistentClasses.Keys[i],
+                    Text = " - " + nn.ExistentClasses.Keys[i],
+                    Top = 20 * (i + 1),
+                    AutoSize = true
+                };
+                lbl.Left = 15 + (int)lbl.Font.Size;
+                this.panelLegend.Controls.Add(lbl);
+
+                Panel panel = new Panel
+                {
+                    Name = "panel" + nn.ExistentClasses.Keys[i],
+                    Top = 20 * (i + 1) + (int)lbl.Font.Size / 2,
+                    Left = 15,
+                    Width = (int)lbl.Font.Size,
+                    Height = (int)lbl.Font.Size,
+                    BackColor = nn.UsedColors[i]
+                };
+                this.panelLegend.Controls.Add(panel);
+            }
+        }
+
+        private void chbVisualization_CheckedChanged(object sender, EventArgs e)
+        {
+            this.nn_EndEpochEvent(this, new EndEpochEventArgs());
+        }
+
+        private void nn_EndEpochEvent(object sender, EndEpochEventArgs e)
+        {
+            Application.DoEvents();
+        }
+
+        private void btnLoadWeight_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(ofd.FileName);
+                string[] line = sr.ReadLine().Split(' ');
+                List<double> values = new List<double>();
+                for (int i = 0; i < line.Length - 1; i++)
+                {
+                    values.Add(Double.Parse(line[i]));
+                }
+                Neuron Winner = nn.FindWinner(values);
+                sofmVisualizer.LightUpThePixel(Winner.Coordinate.X, Winner.Coordinate.Y);
+                lblResult.Text = "Most likely a/an " + findNearestDistance(Winner.Coordinate.X, Winner.Coordinate.Y);
+                ofd.Dispose();
+            }
+        }
+
+        
+    }
+}
